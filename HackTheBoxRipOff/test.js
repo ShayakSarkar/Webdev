4dfb549b49e6c39682923b0b47948b646e62663c
ShayakSarkar <shayaksarkar2000@gmail.com>
2020-01-18 22:12:09 +0530

    removed useless directories

--- HackTheBoxRipOff/test.js
+++
@@ -1,4 +0,0 @@
-elm=document.getElementById("element");
-elm.style.fontFamily="ubuntu";
-elm.style.fontSize="42px"
-elm.animate([{left: "10%"},{left: "20%"},{left: "40%"}],{duration: 1000,iterations: Infinity});
--- travelsite/action.js
+++
@@ -1,8 +0,0 @@
-function submit()
-{
-	let uname=document.getElementById("username").value;
-	let pass=document.getElementById("password").value;
-	let win=window.open("usernameAndPasswordViewer.html");
-	win.document.write("Username: "+uname+" Password: "+pass);
-}
-
